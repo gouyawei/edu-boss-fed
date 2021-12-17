279ce8cf220013e91ecbc45f58ebd310a37a07cf
gouyawei <954032781@qq.com>
2021-12-17 17:42:09 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // 引入路由中需要使用得当组件功能
-// import Login from ../views/login/index'
+import Login from '../views/login/index'
 
 Vue.use(VueRouter)
 
 // 路由规则
 const routes = [
-  // {
-  //   path: '/login',
-  //   name: 'login',
-  //   component: Login
-  // }
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
