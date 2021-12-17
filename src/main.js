ba83aad2c9abccb682d31a986701202d1ddd944f
gouyawei <954032781@qq.com>
2021-12-17 17:26:37 +0800

    master/origin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element-ui 模块包
+import ElementUI from 'element-ui'
+// 引入element 的主题文件
+// import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义的全局样式文件
+import './styles/index.scss'
+// 将element 注册为 Vue 插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 引入路由中需要使用得当组件功能
+// import Login from ../views/login/index'
+
 Vue.use(VueRouter)
 
 // 路由规则
-const routes = []
+const routes = [
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   component: Login
+  // }
+]
 
 const router = new VueRouter({
   routes
